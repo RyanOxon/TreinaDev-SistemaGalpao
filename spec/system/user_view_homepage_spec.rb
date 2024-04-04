e5d5e1516b01dc7281b5a86fe1492c71bdfd8492
Rafael Salgado <rafael.salgado31@gmail.com>
2024-04-04 15:45:33 -0300

    Visitante ve a tela home

---
+++ spec/system/user_view_homepage_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "Usuario visita tela inicial" do
+  it "e ve o nome do app" do
+    #arrange
+    #act
+    visit('/')
+    #assert
+    expect(page).to have_content('Galpões & Estoque')
+  end
+  
+end

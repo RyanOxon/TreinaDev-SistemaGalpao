a2956dbe91434b53201757b5b9b12bce5891b6eb
Rafael Salgado <rafael.salgado31@gmail.com>
2024-04-05 15:20:00 -0300

    Usuario verifica que nao existe galpoes

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -8,5 +8,34 @@ describe "Usuario visita tela inicial" do
     #assert
     expect(page).to have_content('Galpões & Estoque')
   end
+
+  it "e vê os galpoes cadastrados" do
+    #arrange
+    Warehouse.create(name: 'Rio' , code: 'SDU' , city: 'Rio de Janeiro' , area: 60000)
+    Warehouse.create(name: 'Maceio' , code: 'MCZ' , city: 'Maceio' , area: 50000)
+    #act
+    visit('/')
+    #assert
+
+    expect(page).not_to have_content('Não existem galpões cadastrados') 
+    expect(page).to have_content('Rio') 
+    expect(page).to have_content('Código: SDU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('60000m²')
+
+    expect(page).to have_content('Maceio') 
+    expect(page).to have_content('Código: MCZ')
+    expect(page).to have_content('Cidade: Maceio')
+    expect(page).to have_content('50000m²')
+  end
+  
+  it "e nao existem galpões cadastrados" do
+    #arrange
+    #act
+    visit('/')
+    #assert
+    expect(page).to have_content('Não existem galpões cadastrados')  
+  end
+  
   
 end

041658d453c662bff2315df209fb722125aea2d6
Rafael Salgado <rafael.salgado31@gmail.com>
2024-04-13 14:39:16 -0300

    Devise, Log-in, Sign-up e CSS

---
+++ spec/system/user_sign_in_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe "Usuario se autentica" do
+  it "com sucesso" do
+    User.create!(email:"teste@teste.com", password: "password")
+    visit root_path
+    within 'nav' do
+      click_on 'Entrar'
+    end
+    fill_in "E-mail",	with: "teste@teste.com"
+    fill_in "Senha", with: "password"
+    click_on 'Log in'
+    within 'nav' do
+      expect(page).to have_content 'Sair' 
+      expect(page).not_to have_content 'Entrar'
+      expect(page).to have_content 'teste@teste.com' 
+    end
+    expect(page).to have_content 'Login efetuado com sucesso.' 
+  end
+  
+  it "e desloga" do
+    User.create!(email:"teste@teste.com", password: "password")
+    visit root_path
+    within 'nav' do
+      click_on 'Entrar'
+    end
+    fill_in "E-mail",	with: "teste@teste.com"
+    fill_in "Senha", with: "password"
+    click_on 'Log in'
+    click_on 'Sair'
+
+    expect(page).to have_content 'Logout efetuado com sucesso.' 
+    expect(page).to have_content 'Entrar'
+    expect(page).not_to have_content 'Sair'
+    expect(page).not_to have_content 'Usuario: teste@teste.com' 
+  end
+end
---
+++ spec/system/user_sing_up_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "Usuario se cadastra" do
+  it "com sucesso" do
+
+    visit root_path
+    within 'nav' do
+      click_on 'Entrar'
+    end
+    click_on 'Sign up'
+    fill_in "Nome",	with: "Rafael"
+    fill_in "E-mail", with: "rafael@email.com"
+    fill_in "Senha", with: "password"
+    fill_in "Confirme sua senha", with: "password"
+    click_on "Sign up"
+
+    within 'nav' do
+      expect(page).to have_content 'Sair' 
+      expect(page).to have_content 'Usuário: rafael@email.com'
+      expect(page).not_to have_content 'Entrar'  
+    end
+    expect(page).to have_content 'Bem vindo! Você realizou seu registro com sucesso.' 
+  end
+  
+end
